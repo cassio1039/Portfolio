f34ff5f11d2b375e6b808f57d42eb63ad04c1ad6
Cassio <97923417+cassio1039@users.noreply.github.com>
2025-07-10 17:17:28 -0300

    atualizando portfolio

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const texto = "Olá, sou Cássio Martins. Atualmente estou me aprofundando em desenvolvimento front-end com JavaScript e tenho como meta atuar como programador FullStack.";
-  const destino = document.getElementById("digitando");
+let texto = "Olá, sou o Cássio! 😄\n\n"
+texto += "Minha trajetória profissional começou na verdade como motorista de aplicativo! Onde em uma das minhcas corridas conheci um passageiro que trabalhava como desenvolvedor e durante nossa conversa me encantei com a área. Foi ali que despertou meu interesse em programação..\n\n"
+texto += "Atualmente estou me especializando em desenvolvimento front-end com JavaScript e tenho como objetivo atuar como programador FullStack. \n\n";
+texto += "Possuo habilidades práticas com HTML, CSS e JavaScript, além de conhecimentos com versionamento de código Git/Github. Atualmente estou estudando frameworks modernos como React e busco constantemente melhorar meu código e aprender novas tecnologias.";
+  let destino = document.getElementById("digitando");
   let index = 0;
 
   function digitar() {
