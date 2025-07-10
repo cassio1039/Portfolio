7c8f775159bc52658f74d9019d2cd57cb4193c22
Cassio <97923417+cassio1039@users.noreply.github.com>
2025-07-10 17:25:16 -0300

    atualizando perfil

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let texto = "Olá, sou o Cássio! 😄\n\n"
-texto += "Minha trajetória profissional começou na verdade como motorista de aplicativo! Onde em uma das minhas corridas conheci um passageiro que trabalhava como desenvolvedor e durante nossa conversa me encantei com a área. Foi ali que despertou meu interesse em programação..\n\n"
+texto += "Minha trajetória profissional começou na verdade como motorista de aplicativo! Onde em uma das minhas corridas conheci um passageiro que trabalhava como desenvolvedor e durante nossa conversa me interessei pela área. Foi ali que despertou meu interesse em programação..\n\n"
 texto += "Atualmente estou me especializando em desenvolvimento front-end com JavaScript e tenho como objetivo atuar como programador FullStack. \n\n";
 texto += "Possuo habilidades práticas com HTML, CSS e JavaScript, além de conhecimentos com versionamento de código Git/Github. Atualmente estou estudando frameworks modernos como React e busco constantemente melhorar meu código e aprender novas tecnologias.";
   let destino = document.getElementById("digitando");
